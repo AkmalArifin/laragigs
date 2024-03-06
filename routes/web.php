954a6214b2e8166fd891520a74ffb319ddca1079
Akmal Arifin <makmalarifin25@gmail.com>
2024-03-06 23:08:53 +0700

    feat: tutorial model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listings;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,18 +22,14 @@ Route::get('/', function () {
 Route::get('/list', function() {
     return view('listings', [
         "title" => "List Ikan",
-        "data" => [
-            [
-                'id' => 1,
-                'title' => "Ikan Pari",
-                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta tortor vel quam consequat semper. Pellentesque sit amet cursus lectus, ac congue nulla. Proin egestas pellentesque dui, sagittis interdum nulla convallis vitae. Maecenas faucibus ullamcorper gravida. Nam nec pulvinar sem. Ut feugiat luctus nulla, a consectetur arcu sagittis a. Nunc a maximus nibh.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Ikan Hiu',
-                'description' => 'Nullam semper tincidunt erat, eget vulputate dui maximus ac. Donec non dui placerat, maximus odio sit amet, convallis nunc. Curabitur euismod gravida tortor sed interdum. Praesent mattis est quis tellus consectetur, consectetur mollis erat vehicula. Vivamus id neque urna. Proin id massa mollis, condimentum dolor sed, varius sem. Nam eu neque sit amet augue fermentum commodo. Ut sit amet nunc ac orci pellentesque fringilla eget eget orci.'
-            ]
-        ]
+        "data" => Listings::all(),
+    ]);
+});
+
+Route::get('/list/{id}', function($id) {
+    return view('list', [
+        "title" => "Ikan " . $id,
+        "data" => Listings::search($id),
     ]);
 });
 
